d4688795dc680887a7fac60a1dac1677f352ce01
LewisDyer <2299195D@student.gla.ac.uk>
2019-02-15 14:44:48 +0000

    completed initial project setup

---
+++ settle_my_settle/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for settle_my_settle project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "settle_my_settle.settings")
+
+application = get_wsgi_application()
