c6b6e5abd42c13eba243e15432107e9cea12ec00
LukeGall <lukepgall@gmail.com>
2019-02-25 10:36:16 +0000

    Added basic user functions, registration and change password

--- settle_my_settle/settings.py
+++ settle_my_settle/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'settle',
+    'registration'   # Add registration redux
 ]
 
 MIDDLEWARE = [
@@ -119,3 +120,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Let's suers regisiter
+REGISTRATION_OPEN = True
+# Use this if we want email activation, will remove if we don't need it
+# ACCOUNT_ACTIVATION_DAYS = 7
+# Again, not sure how this will work with the login form
+# REGISTRATION_AUTO_LOGIN = True
+# Go to home pages after log in
+LOGIN_REDIRECT_URL = '/settle/'
+# Gonna leave this commented until we work out the login form
+# LOGIN_URL = '/users/login/'
