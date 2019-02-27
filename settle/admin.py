5752cddc5e1f56e1b54b96ac97b45ba8f34eed28
LewisDyer <lewisd251@gmail.com>
2019-02-27 22:27:07 +0000

    Models should be registered + gave them somewhat meaningful names

--- settle/admin.py
+++ settle/admin.py
@@ -3,6 +3,9 @@ from __future__ import unicode_literals
 
 from django.contrib import admin
 
-from settle.models import Tag
+from settle.models import Tag, User, Post, Comment
 
 admin.site.register(Tag)
+admin.site.register(User)
+admin.site.register(Post)
+admin.site.register(Comment)
