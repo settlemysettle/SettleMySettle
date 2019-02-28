07885c2a0053a05448cc7408cbdc1172d1776bbb
Liam Lau <liam.lau@hotmail.co.uk>
2019-02-28 19:49:31 +0000

    Tidied up - removed unneeded pics, mapped urls for nav bar pages

--- settle/urls.py
+++ settle/urls.py
@@ -3,4 +3,7 @@ from settle import views
 
 urlpatterns = [
     url(r'^$', views.index, name="index"),
+    url(r'^feed$', views.feed, name="feed"),
+    url(r'^upload$', views.upload, name="upload"),
+    url(r'^suggest-tag$', views.suggest_tag, name="tags"),
 ]
--- settle/views.py
+++ settle/views.py
@@ -7,5 +7,16 @@ from django.shortcuts import render
 
 def index(request):
     context_dict = {'boldmessage': "Hi from the view for index! :D"}
-
     return render(request, 'settle/index.html', context=context_dict)
+
+def feed(request):
+    context_dict = {}
+    return render(request, 'settle/feed.html', context=context_dict)
+
+def upload(request):
+    context_dict = {}
+    return render(request, 'settle/upload.html', context=context_dict)
+
+def suggest_tag(request):
+    context_dict = {}
+    return render(request, 'settle/suggest-tag.html', context=context_dict)
