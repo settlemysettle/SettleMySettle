382da62741036b65b711a9c4f6ac84b58098277a
Aidan Randtoul <2323851r@student.gla.ac.uk>
2019-03-06 01:19:34 +0000

    Completed and Streamlined current page highlighting functionality

--- static/js/base.js
+++ static/js/base.js
@@ -1,5 +1,12 @@
 // when the document is ready, the functions below can be used
-$(document).ready(function(){
+$(document).ready(function() {
+
+  // Highlight currently selected page
+  $('.nav-link').each(function() {
+    if ($(this).prop('href') == window.location.href) {
+      $(this).addClass('active'); $(this).parents('li').addClass('active');
+    }
+  });
 
   // drop login menu down when login button is clicked
   $("#loginButton").click(function() {
