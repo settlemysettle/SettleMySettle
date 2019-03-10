5768cf99fd7802225d5c5a73d52038de6d98af65
LukeGall <lukepgall@gmail.com>
2019-03-10 17:39:55 +0000

    User model test cases

--- settle/tests/test_models.py
+++ settle/tests/test_models.py
@@ -6,7 +6,7 @@ from settle.fields import ColourField
 
 class TagTestCase(TestCase):
     def create_tag(self, t, c, gt, isP, sa):
-        # Use this to mnake Tag objects
+        # Use this to make Tag objects
         return Tag.objects.create(text=t, colour=c, is_game_tag=gt,
                                   is_pending=isP, steamAppId=sa)
 
@@ -32,9 +32,38 @@ class TagTestCase(TestCase):
         self.asserRaises(
             Exception, Tag, colour="this isn't a valid colour code")
 
+    def test_unique_tag(self):
+        testTag = self.create_tag("DukeIsAGoodBoy", "#FFFFFF", True, False, "")
+        # This should raise an error when we try to make a new one
+        self.assertRaises(Exception, Tag, text="DukeIsAGoodBoy")
+
 
 class UserTestCase(TestCase):
-    print("Todo")
+    def create_user(self, un, pa):
+        # Make using the user model
+        return User.objects.create(username=un, password=pa)
+
+    def test_making_user(self):
+        # Make a user using our method
+        userTest = self.create_user("test", "password")
+        # Check it is an instance of the User class
+        self.asserTue(isinstance(userTest, User))
+
+    def test_max_length_username(self):
+        # make a test user with a username too long
+        self.assertRaises(
+            Exception, User, username="This is a very long username")
+
+    def test_max_length_password(self):
+        # Try to make a user with a very long password
+        self.assertRaises(
+            Exception, User, password="This password is far too long, you will never remember it")
+
+    def test_unique_username(self):
+        userTest = self.create_user("testUser", "password")
+        # Should raise an issue as this is already a user
+        self.assertRaises(Exception, User, username="testUser",
+                          password="password")
 
 
 class PostTestCase(TestCase):
