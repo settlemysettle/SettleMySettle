33d141decdd5ffa9da0e329e598e58fea9889f8d
LukeGall <lukepgall@gmail.com>
2019-03-14 11:45:40 +0000

    Upload view testcase

--- settle/tests/test_views.py
+++ settle/tests/test_views.py
@@ -11,7 +11,7 @@ class IndexViewTestCase(TestCase):
     def test_context_dict(self):
         response = self.client.get(reverse('index'))
         # The context_dict should contain a list of images it will display to the page
-        posts = response.context[-1]['post_list']
+        posts = response.context[-1]['pictures']
         # Should return 6 images
         self.assertTrue(len(posts) == 6)
 
@@ -29,7 +29,7 @@ class feedViewTestCase(TestCase):
         self.user.favourite_games.add(self.tag)
 
     def test_context_dict(self):
-        # Get the response when you use a specfic user
+        # Get the response when you are logged in as a specfic user
         response = self.client.get(reverse('feed'), {'user': self.user})
         # Get the post objects from the context dictionary
         posts = response.context[-1]['post_list']
@@ -38,9 +38,38 @@ class feedViewTestCase(TestCase):
         for post in posts:
             self.assertTrue(post.game_tag == self.tag)
 
+    def test_empty_fav_games(TestCase):
+        user = User.objects.create(username="newUser", password="newPassword")
+        user.save()
+        # Get the response with the new user
+        response = self.client.get(reverse('feed'), {'user': user})
+        # Post list sould be empty as we have no fav games
+        posts = response.context[-1]['post_list']
+        self.assertTrue(len(posts) == 0)
+
 
 class uploadViewTestCase(TestCase):
-    print("todo")
+    def setUp(self):
+        self.client = Client()
+        # Picture we will try to upload
+        self.picture = "logoWAD.png"
+        # Make a new user
+        self.user = User.objects.create(
+            username="test", password="testPassword")
+        self.user.save()
+        # Get a tag object that we will add to the fav games of our new user
+        self.tag = Tag.objects.get(text="Civ 6")
+        self.tag.save()
+        self.description = "test description"
+
+    def test_upload_response(self):
+        # Post a new image using a HTTP post
+        response = self.client.post(
+            reverse('upload'), {'author': self.user, 'picture': self.picture,
+                                'game_tag': self.tag, 'description': self.description})
+        # Check the request form is valid
+        form = response.context[-1]['form']
+        self.assertTrue(form.is_valid())
 
 
 class sugTagViewTestCase(TestCase):
