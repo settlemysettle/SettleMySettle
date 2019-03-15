f7aad17528931b0419af93e18f8a8378b389dab9
LukeGall <lukepgall@gmail.com>
2019-03-15 14:48:14 +0000

    fixes

--- settle/tests/test_views.py
+++ settle/tests/test_views.py
@@ -2,6 +2,7 @@ from django.test import TestCase
 from django.core.urlresolvers import reverse
 from django.test import Client
 from settle.models import User, Tag, Post
+from django.utils import timezone
 
 
 class IndexViewTestCase(TestCase):
@@ -19,14 +20,20 @@ class IndexViewTestCase(TestCase):
 class feedViewTestCase(TestCase):
     def setUp(self):
         self.client = Client()
-        # Get a tag object that we will add to the fav games of our new user
-        self.tag = Tag.objects.get(text="Civ 6")
+        # Make a tag object that we will add to the fav games of our new user
+        self.tag = Tag.objects.create(
+            text="Civ 6", colour="#FFFFFF", is_game_tag=True, is_pending=False, steamAppId=222)
         # Make a new user
         self.user = User.objects.create(
             username="test", password="testPassword")
-        self.user.save()
+        # self.user.save()
         # Add a new tag to it's fav games
         self.user.favourite_games.add(self.tag)
+        self.userTest = User.objects.create(
+            username="testUser", password="password")
+        self.post = Post.objects.create(
+            author=self.userTest, picture='static/images/logoWAS.png', game_tag=self.tag, date_submitted=timezone.now(),
+            description="test")
 
     def test_context_dict(self):
         # Get the response when you are logged in as a specfic user
@@ -40,7 +47,7 @@ class feedViewTestCase(TestCase):
 
     def test_empty_fav_games(self):
         user = User.objects.create(username="newUser", password="newPassword")
-        user.save()
+        # user.save()
         # Get the response with the new user
         response = self.client.get(reverse('feed'), {'user': user})
         # Post list sould be empty as we have no fav games
@@ -57,16 +64,16 @@ class uploadViewTestCase(TestCase):
         self.user = User.objects.create(
             username="test", password="testPassword")
         self.user.save()
-        # Get a tag object that we will add to the fav games of our new user
-        self.tag = Tag.objects.get(text="Civ 6")
-        self.tag.save()
+        # Get a tag object that will be the gametag of the upload
+        self.tag = Tag.objects.create(
+            text="Civ 6", colour="#FFFFFF", is_game_tag=True, is_pending=False, steamAppId=222)
         self.description = "test description"
 
     def test_upload_response(self):
         # Post a new image using a HTTP post
         response = self.client.post(
             reverse('upload'), {'author': self.user, 'picture': self.picture,
-                                'game_tag': self.tag, 'description': self.description})
+                                'game_tag': self.tag, 'description': self.description, 'info_tags': []})
         # Check the request form is valid
         form = response.context[-1]['form']
         self.assertTrue(form.is_valid())
@@ -83,7 +90,7 @@ class sugTagViewTestCase(TestCase):
 
     def test_new_post(self):
         # Send the data as a post request
-        response = self.client.post(reverse('suggest-tag'),
+        response = self.client.post(reverse('tags'),
                                     {'text': self.text, 'colour': self.colour, 'is_game_tag': self.is_game_tag,
                                      'steamAppId': self.steamAppId})
         # CHeck the data we sent is valid
@@ -93,8 +100,14 @@ class sugTagViewTestCase(TestCase):
 
 class postViewTestCase(TestCase):
     def setUp(self):
-        # Placeholder untill I can choose a post
-        self.post = list(Post.objects.all())[0]
+        # Need a user and a tag object
+        self.userTest = User.objects.create(
+            username="test", password="password")
+        self.tag = Tag.objects.create(
+            text="Civ 6", colour="#FFFFFF", is_game_tag=True, is_pending=False, steamAppId=222)
+        self.post = Post.objects.create(
+            author=self.userTest, picture='static/images/logoWAS.png', game_tag=self.tag, date_submitted=timezone.now(),
+            description="test")
         self.client = Client()
 
     def test_look_at_post(self):
