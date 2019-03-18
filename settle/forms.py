cea4abb061911f14a4360a0ea6f5ed1161c9e5d6
LewisDyer <lewisd251@gmail.com>
2019-03-18 18:52:03 +0000

    Placed checkbox list inside scrollbar (w/problem)

--- settle/forms.py
+++ settle/forms.py
@@ -54,7 +54,7 @@ class UploadForm(forms.ModelForm):
     game_tag = forms.ModelChoiceField(queryset=Tag.objects.filter(is_game_tag=True).filter(is_pending=False).order_by("text"))
 
 
-    info_tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.filter(is_game_tag=False).filter(is_pending=False).order_by("text"), widget=forms.CheckboxSelectMultiple)
+    info_tags = forms.ModelMultipleChoiceField(queryset=Tag.objects.all().order_by("text"), widget=forms.CheckboxSelectMultiple(attrs = {'class':'info-tag-list'}))
     class Meta:
         # Make it inherit fields from Post model
         model = Post
