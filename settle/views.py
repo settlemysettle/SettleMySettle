168628b1526d09566ca5ce44d4791ae49fc79e31
LewisDyer <lewisd251@gmail.com>
2019-03-19 21:32:39 +0000

    suggesting tag as admin makes it non-pending now

--- settle/views.py
+++ settle/views.py
@@ -251,9 +251,16 @@ def suggest_tag(request):
     context_dict = {}
 
     if request.method == "POST":
-        if request.POST.get('type') == 'suggest':
-            suggest_tags_form = SuggestTag(request.POST)
+        if request.POST.get('type') in ['suggest', 'approve']:
+         
+            if request.POST.get('type') == 'approve':
+                text = request.POST.get('text')
 
+                if Tag.objects.filter(text=text).exists():
+                    t = Tag.objects.filter(text=text).delete()
+
+            suggest_tags_form = SuggestTag(request.POST)
+            
             if suggest_tags_form.is_valid():
                 new_tag = suggest_tags_form.save(commit=False)
                 new_tag.is_pending = True
