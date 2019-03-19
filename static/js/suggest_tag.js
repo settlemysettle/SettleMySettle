70e03bb1939a6d6478316f2b96e2c00bb0e191cd
LewisDyer <lewisd251@gmail.com>
2019-03-19 10:47:31 +0000

    Implemented tag preview with auto-update

--- static/js/suggest_tag.js
+++ static/js/suggest_tag.js
@@ -1,5 +1,13 @@
 $(document).ready(function () {
-    $('#suggest-form form').change(function () {
+    $('#suggest-form').on('keyup change paste', ':input', function () {
         console.log("inputs change!");
+
+        var tagText = $('#id_text').val();
+        var tagCol = $('#id_colour').val();
+
+        console.log(tagText + ", " + tagCol);
+
+        $('#preview-tag').css('background-color', tagCol);
+        $('#preview-tag').text(tagText);
     });
 });
