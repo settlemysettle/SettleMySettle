834425bebb88e528f2023f3d840107a898147dcb
LewisDyer <lewisd251@gmail.com>
2019-03-19 11:15:31 +0000

    removed some extraneous logging

--- static/js/suggest_tag.js
+++ static/js/suggest_tag.js
@@ -1,13 +1,9 @@
 $(document).ready(function () {
     $('#suggest-form').on('keyup change paste', ':input', function () {
-        console.log("inputs change!");
-
         var tagText = $('#id_text').val();
         var tagCol = $('#id_colour').val();
         var steamAppId = $('#id_steamAppId').val();
-
-        console.log(tagText + ", " + tagCol);
-
+        
         $('#preview-tag').css('background-color', tagCol);
         $('#preview-tag').text(tagText);
         $('#preview-steam').attr('href', "https://store.steampowered.com/app/" + steamAppId);
