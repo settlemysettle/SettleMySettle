746059755a61b7b07b7c169a580b0369ab06e774
LewisDyer <lewisd251@gmail.com>
2019-03-21 19:05:49 +0000

    technically asynchronous (with issues)

--- static/js/post.js
+++ static/js/post.js
@@ -1,8 +1,26 @@
 $(document).ready(function () {
 
     console.log("loaded!");
-    $('.like_button').submit(function () {
+    $('.like_button').on('submit', function (event) {
+        event.preventDefault();
         console.log('Trying to like...');
+
+        $.ajax({
+            url: $(this).attr('action'),
+            type: $(this).attr('method'),
+            data: $(this).serialize(),
+
+            success: function(response) {
+                console.log(response);
+                $("html").html(response);
+            },
+
+            error: function(request, status, error) {
+                console.log(request.responseText);
+            } 
+
+
+        });
     });
 
 });
